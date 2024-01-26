5cd6f36ce87fc7bafd1040e1135f9e66629b09b1
sherifyyy <126975995+sherifyyy@users.noreply.github.com>
2024-01-26 18:59:10 +0200

    added gemstones

---
+++ gemstones_and_grasses_plus_more.js
@@ -0,0 +1,12 @@
+elements.diamond = {
+	color: "#b9f2ff",
+	behavior: behaviors.solid,
+	category: "solids",
+	state: "solid"
+};
+elements.sapphire = {
+	color: "#0F52BA",
+	behavior: behaviors.solid,
+	category: "solids",
+	state: "solid"
+};
